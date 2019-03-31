f4d93f006963f21847be3c367564fb36d6ec77a4
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-31 10:14:29 +0430

    Simple RecyclerView Complete

--- app/src/main/java/com/example/simplerecyclerview/ContactAdapter.java
+++ app/src/main/java/com/example/simplerecyclerview/ContactAdapter.java
@@ -2,8 +2,11 @@ package com.example.simplerecyclerview;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactViewHolder>
 {
@@ -16,11 +19,16 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
     @NonNull
     @Override
     public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        LayoutInflater inflater= LayoutInflater.from(viewGroup.getContext());
+        View view = inflater.inflate(R.layout.list_item_layout,viewGroup,false);
+        ContactViewHolder vh = new ContactViewHolder(view);
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactViewHolder contactViewHolder, int i) {
+            String title = data[i];
+            contactViewHolder.tvTitle.setText(title);
 
     }
 
@@ -31,8 +39,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactV
 
     public static  class ContactViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView imgIcon;
+        TextView tvTitle;
         public ContactViewHolder(@NonNull View itemView) {
             super(itemView);
+            imgIcon =itemView.findViewById(R.id.imgIcon);
+            tvTitle= itemView.findViewById(R.id.tvTitle);
+
         }
     }
 }
--- app/src/main/java/com/example/simplerecyclerview/MainActivity.java
+++ app/src/main/java/com/example/simplerecyclerview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.simplerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView contactList = findViewById(R.id.contactList);
+        contactList.setLayoutManager(new LinearLayoutManager(this));
+        String[] contactNames = {"Sadeq Salehi", "Samir Abbas Fard", "Ali Dris", "Hani Faisali", "Nasser Sawaedi", "Afshin Shokrollahi",
+                "Abdul Jabbar Alizadeh", "Jasim Thameri", "Abdul Wahab Rubaihawi"};
+        contactList.setAdapter(new ContactAdapter(contactNames));
+
 
     }
 }
